73cf817f9108322b8aca31a7f06529c58c12fb35
Daniel Manuel <dmanueljr@gmail.com>
2015-06-23 23:17:36 -0700

    adds parking lots to map in clusters

--- parkspy/app/assets/javascripts/application.js
+++ parkspy/app/assets/javascripts/application.js
@@ -15,18 +15,97 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-function initialize() {
-	console.log("initializing")
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
 
-  var map = new google.maps.Map(document.getElementById('map'),
-      mapOptions);
-  console.log("initialized")
+//start google maps
+
+
+function initialize() {
+  	console.log("initializing")
+
+    //sets center to city of santa monica
+    var center = new google.maps.LatLng(34.0218628, -118.4804206)
+
+    // sets map style
+    var mapStyle = [
+          {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+              { "visibility": "on" },
+              { "color": "#808080" },
+              { "weight": 0.1 }
+            ]
+          }
+        ]  
+
+    //sets map options
+        var mapOptions = {
+          center: center,
+          zoom: 10,
+          mapTypeControlOptions: {
+              mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'santamonicamap']
+          }
+        };
+
+    //html element place holder for google map
+    var map = new google.maps.Map(document.getElementById('map'),
+          mapOptions);
+
+    //aplies style to map
+    var styledMapOptions = {
+      name: "Santa Monica Map"
+    };
+    var SantaMonicaRoadMapType = new google.maps.StyledMapType(
+        mapStyle, styledMapOptions);
+    map.mapTypes.set('santamonicamap', SantaMonicaRoadMapType);
+    map.setMapTypeId('santamonicamap');
+
+    console.log("initialized")
+
+    //adds traffic layer to map
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    //API call on parking lots/structures
+    var lots = [];
+    $.ajax("https://parking.api.smgov.net/lots/", {
+        success: function(data) {
+            lots = data;
+            // console.log(lots.length);
+
+            //collects and plots parking lots/structures on the map in clusters
+            var lotMarkers = [];
+            for (var i = 0; i < lots.length; i++) {
+                var lotData = lots[i];
+                var lotImage = '/assets/lot-icon.png';
+                var lotPosition = new google.maps.LatLng(lotData.latitude, lotData.longitude);
+                var lotMarker = new google.maps.Marker({
+                    position: lotPosition,
+                    map: map,
+                    icon: lotImage
+                });
+                lotMarkers.push(lotMarker);
+                getLotData(lotData, lotMarker);
+            }
+            var lotMarkerCluster = new MarkerClusterer(map, lotMarkers);
+        }
+    });
+
+
+
+
+
 }
 
+
+
+
+
+
+
+
+
+//initializer
 function loadScript() {
 	console.log("loading script")
   var script = document.createElement('script');

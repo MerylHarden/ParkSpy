0ed8bf7eee18fd892abe28e73a2613b9a46d61b0
MerylHarden <merylharden@gmail.com>
2015-06-26 14:06:55 -0700

    added Travis parkspy test

---
+++ spec/features/items_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+
+
+feature "a user can find a parking spot using the search bar", :type => :feature do
+  #scenario "valid item" do
+  #   visit '/items/new'
+  #   fill_in "Name", with: "My new item"
+  #   fill_in "Qty", with: "1"
+  #   click_button "Create Item"
+  #   expect(page).to have_content("My new item")
+  # end
